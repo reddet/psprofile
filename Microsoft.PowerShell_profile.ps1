748bb68937efd931e0ef558009c82bd7d6b28f5a
Keith <reddet@gmail.com>
2019-02-02 12:31:35 -0600

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,20 @@
 <#
 Powershell Profile
-Keith Tedder
-12\27\2018
+KET
+02\02\2019
 #>
 
 Set-Location C:\PS1
 
-$host.ui.RawUI.WindowTitle = "Mjollnir"
+#Powershell Logging
 
+Start-Transcript C:\PS1\PSLogs\log$YYYYMMDD.txt -Append
+function Close-Powershell { if ($Error.Count -gt 0) { $Error | Out-File c:\PS1\PSLogs\errors$YYYYMMDD.txt -Append } Stop-Transcript exit
+}
+#UI Modification
+$host.ui.RawUI.WindowTitle = "($env:userdomain\$env:username) Windows PowerShell"
+
+#Alias items
 new-item alias:np -value C:\Windows\System32\notepad.exe
+new-item alias:npp -value "C:\Program Files (x86)\Notepad++\notepad++.exe"
 
