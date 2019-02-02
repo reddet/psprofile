04b4ddf5449ada915ed52607ce33fb1063033ef2
Keith <reddet@gmail.com>
2019-02-02 13:16:06 -0600

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,10 +7,10 @@ KET
 Set-Location C:\PS1
 
 #Powershell Logging
-
-Start-Transcript C:\PS1\PSLogs\log$YYYYMMDD.txt -Append
-function Close-Powershell { if ($Error.Count -gt 0) { $Error | Out-File c:\PS1\PSLogs\errors$YYYYMMDD.txt -Append } Stop-Transcript exit
+Start-Transcript C:\PS1\PSLogs\log$YYYYMMDD.log -Append -Force
+function Close-Powershell { if ($Error.Count -gt 0) { $Error | Out-File c:\PS1\PSLogs\errors$YYYYMMDD.log -Append } Stop-Transcript exit
 }
+
 #UI Modification
 $host.ui.RawUI.WindowTitle = "($env:userdomain\$env:username) Windows PowerShell"
 
@@ -18,3 +18,5 @@ $host.ui.RawUI.WindowTitle = "($env:userdomain\$env:username) Windows PowerShell
 new-item alias:np -value C:\Windows\System32\notepad.exe
 new-item alias:npp -value "C:\Program Files (x86)\Notepad++\notepad++.exe"
 
+#Functions
+function New-Password {C:\PS1\PSScripts\ComplexPassword.ps1}
