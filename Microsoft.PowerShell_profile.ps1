ba598b2213b0c8129874f0bd6140dda975c55009
Keith <reddet@gmail.com>
2019-03-17 11:55:42 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,21 +1,115 @@
 <#
 Powershell Profile
 KET
-02\02\2019
+03\17\2019
 #>
 
+<#
+	.WHOIS
+	Keith Tedder
+	
+	.NOTES
+		Added Function New-Password (Password Generator)
+		Added Function Test-PasswordComplexity
+	
+	.SYNOPSIS
+		Powershell Profile
+		
+	.DESCRIPTION
+	
+	.EXAMPLE
+	
+	.LINK
+		
+#>
+
+
+################################################################################
+# Custom PS Location
 Set-Location C:\PS1
 
-#Powershell Logging
-Start-Transcript C:\PS1\PSLogs\log$(get-date -format 'MMddyyyy').log -Append
+################################################################################
+#Custom PS Color Scheme (WIP)
+#[console]::BackgroundColor = "black"
+#
+################################################################################
+# Script Directory
+$psdir="${env:tools}"
 
 
-#UI Modification
-$host.ui.RawUI.WindowTitle = "($env:userdomain\$env:username) Windows PowerShell"
+################################################################################
+# 'autoload' Scripts
+get-childitem "$psdir\autoload\*.ps1" | %{.$_}
 
-#Alias items
+
+################################################################################
+#Console Customizations
+$host.ui.RawUI.WindowTitle = "($env:userdomain\Bifrost) Windows PowerShell" 
+
+Function Prompt
+{
+    $promptString = "PS " + $(Get-Location) + ">"
+    Write-Host $promptString -NoNewline -ForegroundColor Yellow
+    return " "
+}
+
+################################################################################
+# Begin Functions
+
+
+
+# Password Generator ###########################################################
+Function New-Password {
+    param(
+        [int] $Length = 16,
+        [string[]]$ASCII = ('48-57','65-90','97-122','33','45','46','63')
+    )
+    $pwd = ''
+
+    $Ascii_codes = $Null 
+    $ASCII | %{ $Ascii_codes += Invoke-expression $_.replace('-','..') }
+    $chars = @() 
+    $Ascii_codes | %{ $chars += [char][byte] $_ }
+
+    1..$Length | %{
+        $pwd += $chars | Get-Random
+    }
+
+    if (-not $(Test-PasswordComplexity -Password $pwd)) {
+        $pwd = New-Password -Length $Length -ASCII $ASCII
+    }
+	set-clipboard -Value $pwd
+    Write-Output $pwd
+}
+
+
+
+Function Test-PasswordComplexity {
+    param(
+        [string]$Password
+    )
+
+    $isGood = 0
+    # Check for special chars
+    if ($Password -match '[^a-zA-Z0-9]') { $isGood++ }
+    if ($Password -match '[0-9]') { $isGood++ }
+    if ($Password -cmatch '[a-z]') { $isGood++ }
+    if ($Password -cmatch '[A-Z]') { $isGood++ }
+    if ($isGood -ge 3) { Write-Output $true }
+    else { Write-Output $false }
+}
+
+	
+	
+
+
+# Password Generator END########################################################
+# End Functions
+################################################################################
+# Alias
 new-item alias:np -value C:\Windows\System32\notepad.exe
-new-item alias:npp -value "C:\Program Files (x86)\Notepad++\notepad++.exe"
 
-#Functions
-function New-Password {C:\PS1\PSScripts\ComplexPassword.ps1}
+Write-Host "The Skybridge Awaits Your Command" -foregroundcolor green
+
+
+################################################################################
